b3a6c13449045daa1ca733a31eb83d1d93f39399
Cedrick Garcia <icedgarcia@gmail.com>
2023-11-08 19:32:08 +0800

    login ui

--- login.php
+++ login.php
@@ -77,6 +77,7 @@ if (isset($_POST['login'])) {
 include('includes/header.php');
 include('includes/nav.php');
 ?>
+<<<<<<< HEAD
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
   *{
@@ -272,4 +273,115 @@ include('includes/nav.php');
         </form>
     </div>
 </div>
+=======
+<section class="login-section">
+   <div id="overlay">
+      <div class="login-form-wrapper">
+         <div class="left-panel">
+            <a href="">
+               <h1>COMS</h1>
+            </a>
+         </div>
+         <div class="right-panel">
+            <form action="" class="login-form shadow-lg p-4" method="POST">
+               <div class="form-group form">
+                  <input
+                     type="text"
+                     class="form-control form-input"
+                     name="uemail"
+                     id="uemail"
+                     autocomplete="off"
+                     placeholder=""
+                     />
+                  <label for="uemail" class="form-label">
+                  <i class="fa-solid fa-envelope"></i>
+                  Email</label
+                     >
+               </div>
+               <div class="form-group form">
+                  <input
+                     type="password"
+                     class="form-control form-input"
+                     name="upassword"
+                     id="upassword"
+                     placeholder=""
+                     />
+                  <label for="upassword" class="form-label">
+                  <i class="fas fa-lock"></i> Password</label
+                     >
+               </div>
+               <button
+                  type="submit"
+                  class="btn border-danger text-danger mt-3 btn-block shadow-sm font-weight-bold"
+                  name="login"
+                  >
+               Login
+               </button>
+               <div class="text-center">
+                  <h6 class="mt-5">Dont have an account?</h6>
+                  <button class="btn-cta align-center" name="register">
+                  <a class="text-white text-center" href="register.php"> Sign Up </a>
+                  </button>
+               </div>
+            </form>
+         </div>
+      </div>
+   </div>
+   <!-- <h2 class="text-center">Login</h2>
+      <form action="" class="shadow-lg p-4" method="POST">
+       
+      
+      
+      
+        <div class="form-group form">
+          <input
+            type="text"
+            class="form-control form-input"
+            name="uemail"
+            id="uemail"
+            autocomplete="off"
+            placeholder=""
+          />
+          <label for="uemail" class="form-label">
+            <i class="fa-solid fa-envelope"></i>
+            Email</label
+          >
+        </div>
+        <div class="form-group form">
+          <input
+            type="password"
+            class="form-control form-input"
+            name="upassword"
+            id="upassword"
+            placeholder=""
+          />
+          <label for="upassword" class="form-label">
+          <i class="fas fa-lock"></i>               Password</label
+          >
+        </div>
+      
+        <button
+          type="submit"
+          class="btn border-danger text-danger mt-3 btn-block shadow-sm font-weight-bold"
+          name="login"
+        >
+          Login
+        </button>
+      
+        <div class="text-center">
+      
+            <h6 class=" mt-5">Dont have an account?</h6>      
+            <button
+            class="btn-cta align-center"
+            name="register"
+            >
+            <a class="text-white text-center" href="register.php">
+                
+                Sign Up
+              </a>
+          </button>  
+      </div>
+      </form> -->
+</section>
+>>>>>>> 6f40e3e (login ui)
 <?php include('includes/footer.php')?>
