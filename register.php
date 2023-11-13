882f198db998e963ed4833f6b175ab1c7798ec3b
Tugs <rolandtugaoen@gmail.com>
2023-11-13 17:43:51 +0800

    register new ui

--- register.php
+++ register.php
@@ -1,4 +1,13 @@
-<!-- ******************** -->
+<!DOCTYPE html>
+<!-- Created By CodingLab - www.codinglabweb.com -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title> Responsive Registration Form | CodingLab </title>
+    <link rel="stylesheet" href="style.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <!-- ******************** -->
 <!-- ***START SESSION**** -->
 <!-- ******************** -->
 
@@ -99,11 +108,164 @@ if (isset($_POST['register'])) {
     }
 }
 ?>
+<<<<<<< HEAD
 
+=======
+   <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins',sans-serif;
+}
+body{
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  background: linear-gradient(135deg, #71b7e6, #9b59b6);
+}
+.container{
+  max-width: 700px;
+  width: 100%;
+  background-color: #fff;
+  padding: 25px 30px;
+  border-radius: 5px;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.15);
+}
+.container .title{
+  font-size: 25px;
+  font-weight: 500;
+  position: relative;
+}
+.container .title::before{
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 3px;
+  width: 30px;
+  border-radius: 5px;
+  background: linear-gradient(135deg, #71b7e6, #9b59b6);
+}
+.content form .user-details{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 20px 0 12px 0;
+}
+form .user-details .input-box{
+  margin-bottom: 15px;
+  width: calc(100% / 2 - 20px);
+}
+form .input-box span.details{
+  display: block;
+  font-weight: 500;
+  margin-bottom: 5px;
+}
+.user-details .input-box input{
+  height: 45px;
+  width: 100%;
+  outline: none;
+  font-size: 16px;
+  border-radius: 5px;
+  padding-left: 15px;
+  border: 1px solid #ccc;
+  border-bottom-width: 2px;
+  transition: all 0.3s ease;
+}
+.user-details .input-box input:focus,
+.user-details .input-box input:valid{
+  border-color: #9b59b6;
+}
+ form .use-details .use-title{
+  font-size: 20px;
+  font-weight: 500;
+ }
+ form .category{
+   display: flex;
+   width: 80%;
+   margin: 14px 0 ;
+   justify-content: space-between;
+ }
+ form .category label{
+   display: flex;
+   align-items: center;
+   cursor: pointer;
+ }
+ form .category label .dot{
+  height: 18px;
+  width: 18px;
+  border-radius: 50%;
+  margin-right: 10px;
+  background: #d9d9d9;
+  border: 5px solid transparent;
+  transition: all 0.3s ease;
+}
+ #dot-1:checked ~ .category label .one,
+ #dot-2:checked ~ .category label .two,
+ #dot-3:checked ~ .category label .three{
+   background: #9b59b6;
+   border-color: #d9d9d9;
+ }
+ form input[type="radio"]{
+   display: none;
+ }
+ form .button{
+   height: 45px;
+   margin: 35px 0
+ }
+ form .button input{
+   height: 100%;
+   width: 100%;
+   border-radius: 5px;
+   border: none;
+   color: #fff;
+   font-size: 18px;
+   font-weight: 500;
+   letter-spacing: 1px;
+   cursor: pointer;
+   transition: all 0.3s ease;
+   background: linear-gradient(135deg, #71b7e6, #9b59b6);
+ }
+ form .button input:hover{
+  /* transform: scale(0.99); */
+  background: linear-gradient(-135deg, #71b7e6, #9b59b6);
+  }
+ @media(max-width: 584px){
+ .container{
+  max-width: 100%;
+}
+form .user-details .input-box{
+    margin-bottom: 15px;
+    width: 100%;
+  }
+  form .category{
+    width: 100%;
+  }
+  .content form .user-details{
+    max-height: 300px;
+    overflow-y: scroll;
+  }
+  .user-details::-webkit-scrollbar{
+    width: 5px;
+  }
+  }
+  @media(max-width: 459px){
+  .container .content .category{
+    flex-direction: column;
+  }
+}
+   </style>
+<body>
+>>>>>>> 873d5e8 (register new ui)
 <?php
 include('includes/header.php');
 include('includes/nav.php');
 ?>
+<<<<<<< HEAD
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
   *{
@@ -256,6 +418,8 @@ include('includes/nav.php');
     }
   }
 </style>
+=======
+>>>>>>> 873d5e8 (register new ui)
   <div class="container" style="margin-top: 5%;">
     <div class="title">Registration</div>
     <div class="content">
@@ -265,7 +429,11 @@ include('includes/nav.php');
           if (isset($error_message)) {
               echo $error_message;
           }
+<<<<<<< HEAD
 ?>
+=======
+        ?>
+>>>>>>> 873d5e8 (register new ui)
       </p>
 
         <div class="user-details">
@@ -291,9 +459,15 @@ include('includes/nav.php');
         </div>
         
         <div class="use-details">
+<<<<<<< HEAD
           <input type="radio" value="Owner" name="usertype" id="dot-1" required>
           <input type="radio" value="Tenant" name="usertype" id="dot-2" required>
           <input type="radio" value="Accountant" name="usertype" id="dot-3" required>
+=======
+          <input type="radio" value="Owner" name="usertype" id="dot-1">
+          <input type="radio" value="Tenant" name="usertype" id="dot-2">
+          <input type="radio" value="Accountant" name="usertype" id="dot-3">
+>>>>>>> 873d5e8 (register new ui)
           <span class="use-title">User Type</span>
           <div class="category">
             <label for="dot-1">
@@ -318,4 +492,9 @@ include('includes/nav.php');
     </div>
   </div>
 
+<<<<<<< HEAD
 <?php include('includes/footer.php')?>
+=======
+</body>
+</html>
+>>>>>>> 873d5e8 (register new ui)
